0eb25ead147a5cdfa0d8d8f96a5f4b5e41663212
Kartik Shandilya <kartik.shandilya@branch.io>
2021-03-06 18:25:39 +0530

    tada first commit

---
+++ www/js/index.js
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+var app = {
+  initialize: function () {
+    this.bindEvents();
+  },
+  bindEvents: function () {
+    document.addEventListener("deviceready", this.onDeviceReady, false);
+    document.addEventListener("resume", this.onDeviceResume, false);
+  },
+  onDeviceReady: function () {
+    app.handleBranch();
+  },
+  onDeviceResume: function () {
+    app.handleBranch();
+  },
+  handleBranch: function () {
+    Branch.setDebug(true);
+    // Branch initialization
+    Branch.initSession().then(function (data) {
+      if (data["+clicked_branch_link"]) {
+        // read deep link data on click
+        alert("Deep Link Data: " + JSON.stringify(data));
+      }
+    });
+  },
+};
+
+app.initialize();
